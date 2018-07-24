1d622e9a13e3bcb4cfe9c1e138640cb89fc06171
Jianmin Chen <jianminchen.fl@gmail.com>
2018-07-24 15:52:11 -0700

    one week practice

---
+++ By Date/2018 July 17 - July 24 One week Practice/438 Find All anagrams in a string.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode438_FindAllAnagramsInAString
+{
+    /// <summary>
+    /// Leetcode 438 - find all anagrams in a string 
+    /// https://leetcode.com/problems/find-all-anagrams-in-a-string/description/
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int SIZE = 26; 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static IList<int> FindAnagrams(string s, string p)
+        {
+            if (s == null || p == null || s.Length < p.Length)
+                return new List<int>();
+
+            var sLength = s.Length;
+            var pLength = p.Length;
+
+            var source = getCountingSort(p);
+            var counting = getCountingSort(s.Substring(0, pLength));
+            var anagrams = new List<int>();
+            if (checkAnagram(source, counting))
+            {
+                anagrams.Add(0);
+            }
+
+            for (int i = 1; i < sLength && (pLength + i - 1) < sLength; i++)
+            {
+                counting[s[i - 1] - 'a']--;
+                counting[s[pLength + i - 1] - 'a']++;
+                if (checkAnagram(source, counting))
+                {
+                    anagrams.Add(i);
+                }
+            }
+
+            return anagrams;
+        }
+
+        private static bool checkAnagram(int[] source, int[] second)
+        {
+            for (int i = 0; i < SIZE; i++)
+            {
+                if (source[i] != second[i])
+                    return false; 
+            }
+
+            return true; 
+        }
+
+        private static int[] getCountingSort(string p)
+        {
+            if (p == null)
+            {
+                return new int[0];
+            }
+
+            var counting = new int[SIZE];
+            foreach(var item in p)
+            {
+                counting[item - 'a']++;
+            }
+
+            return counting;
+        }
+    }
+}
---
+++ By Date/2018 July 17 - July 24 One week Practice/Leetcode 387 Find first unique character in string.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcdoe387_FindFirstUniqueCharactersInAString
+{
+    /// <summary>
+    /// Leetcode 387 - find first unique characters in a string 
+    /// https://leetcode.com/problems/first-unique-character-in-a-string/description/
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = FirstUniqChar("leetcode");
+        }
+
+        /// <summary>
+        /// time complexity: O(N)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static int FirstUniqChar(string s)
+        {
+            if (s == null || s.Length == 0)
+                return -1; 
+
+            int SIZE   = 26;
+            var count = new int[SIZE];
+            var length = s.Length;
+            for (int i = 0; i < length; i++)
+            {
+                count[s[i] - 'a']++;
+            }
+
+            // find first one with count = 1
+            var hashset = new HashSet<char>(s.ToCharArray());
+            for (int i = 0; i < length; i++)
+            {
+                var current = s[i];
+                if (hashset.Contains(current))
+                {
+                    if (count[current - 'a'] == 1)
+                        return i;
+
+                    hashset.Remove(current); 
+                }
+            }
+
+            return -1;
+        }
+    }
+}
