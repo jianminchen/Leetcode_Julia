058d3aedf33e710aa86c88e80ad086235acf1b85
Jianmin Chen <jianminchen.fl@gmail.com>
2018-07-25 15:46:53 -0700

    check in source code

---
+++ By Date/2017 July -/Leetcode 538 Convert BST to greater tree.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_538_convert_BST_to_greater_tree
+{
+    class Program
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+
+        static void Main(string[] args)
+        {
+            var node  = new TreeNode(5);
+            var node2 = new TreeNode(13);
+            var node3 = new TreeNode(2);
+
+            node.left = node3;
+            node.right = node2;
+
+            var root = ConvertBST(node);
+        }
+
+        public static TreeNode ConvertBST(TreeNode root)
+        {
+            var suffixSum = 0;
+
+            return convertBST_RightRootLeftTraverse(root, ref suffixSum);
+        }
+
+        private static TreeNode convertBST_RightRootLeftTraverse(TreeNode root, ref int suffixSum)
+        {
+            if (root == null)
+                return root; 
+
+            // traverse the tree using right, root, left order 
+            convertBST_RightRootLeftTraverse(root.right, ref suffixSum);
+            var addValue = root.val; // bug catched by online judge, issue with sample test case
+
+            root.val = root.val + suffixSum;
+            suffixSum += addValue;
+            convertBST_RightRootLeftTraverse(root.left, ref suffixSum);
+
+            return root;
+        }
+    }
+}
