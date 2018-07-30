3068fce3f0d7a49a96866f0077b41a120ce8e6b0
Jianmin Chen <jianminchen.fl@gmail.com>
2018-07-30 11:01:47 -0700

    check in

---
+++ By Date/2018 July 25 - July 31 Tree algorithms/Leetcode 617 Merge two binary trees.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_617_Merge_Two_binary_trees
+{
+    /// <summary>
+    /// Leetcode 617 
+    /// 
+    /// </summary>
+    class Program
+    {
+        public class TreeNode {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+
+        public TreeNode MergeTrees(TreeNode t1, TreeNode t2)
+        {
+            if (t1 == null && t2 == null)
+                return null;
+
+            if (t1 == null)
+                return t2;
+            if (t2 == null)
+                return t1;
+
+            t1.val = t1.val + t2.val;
+
+            t1.left = MergeTrees(t1.left,  t2.left);
+            t1.right = MergeTrees(t1.right, t2.right);
+
+            return t1; 
+        }
+    }
+}
