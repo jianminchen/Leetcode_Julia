ce7b9c2313961cbb7fe785bd95b4467738939da4
Jianmin Chen <jianminchen.fl@gmail.com>
2018-08-14 14:52:17 -0700

    check in

---
+++ By Date/2018 August 9 - Sept 1/Leetcode 590 N-ary Tree Postorder Traversal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_590_N_ary_Tree_Postorder_Traversal
+{
+    class Program
+    {
+        public class Node
+        {
+            public int val;
+            public IList<Node> children;
+
+            public Node() { }
+            public Node(int _val, IList<Node> _children)
+            {
+                val = _val;
+                children = _children;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+
+        public IList<int> Postorder(Node root)
+        {
+            var nodes = new List<int>();
+            if (root == null)
+                return nodes;
+
+            postorderTraversal(root, nodes);
+
+            return nodes;
+        }
+
+        /// <summary>
+        /// assume root is not null
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="nodes"></param>
+        private static void postorderTraversal(Node root, List<int> nodes)
+        {           
+            if (root.children == null)
+                return;
+
+            foreach (var item in root.children)
+            {
+                postorderTraversal(item, nodes);
+            }
+
+            nodes.Add(root.val);
+        }
+    }
+}
