d95d5a294f56a13090e7d1f5ab884fe53a744489
Jianmin Chen <jianminchen.fl@gmail.com>
2018-08-14 11:51:19 -0700

    check in

---
+++ By Tags/Tree/Easy Tree Tag/Leetcode 589 N-ary Tree Preorder Traversal.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_589_N_ary_Tree_Preorder_Traversal
+{
+    class Program
+    {
+        public class Node
+        {
+            public int val;
+            public IList<Node> children;
+
+            public Node() { }
+            public Node(int _val, IList<Node> _children)
+            {
+                val = _val;
+                children = _children;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+
+        public static IList<int> Preorder(Node root)
+        {
+            var nodes = new List<int>();
+            if (root == null)
+                return nodes;
+
+            preorderTraversal(root, nodes);
+
+            return nodes;
+        }
+
+        /// <summary>
+        /// assume root is not null
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="nodes"></param>
+        private static void preorderTraversal(Node root, List<int> nodes)
+        {
+            nodes.Add(root.val);
+
+            if(root.children == null)
+                return; 
+
+            foreach(var item in root.children)
+            {
+                preorderTraversal(item, nodes); 
+            }
+        }
+    }
+}
