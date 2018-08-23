bab6eb9ddb0c5cb69d579b17d97eade6951e6862
Jianmin Chen <jianminchen.fl@gmail.com>
2018-08-23 11:03:24 -0700

    check in

---
+++ source code/Leetcode 268 missing number.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_268_Missing_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int MissingNumber(int[] nums)
+        {
+            if (nums == null)
+                return -1;
+
+            var length = nums.Length;
+
+            var missing = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                missing ^= nums[i] ^ i;
+            }
+
+            missing ^= length;
+
+            return missing;
+        }
+    }
+}
