482ad24c9889073233fae143b5e5c65881696f2a
Jianmin Chen <jianminchen.fl@gmail.com>
2018-08-23 12:07:05 -0700

    check in

---
+++ By Tags/Array/Easy/Leetcode 66 plus one - 2018.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_66_plus_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="digits"></param>
+        /// <returns></returns>
+        public int[] PlusOne(int[] digits)
+        {
+            if (digits == null)
+                return new int[0];
+
+            var length = digits.Length;
+
+            for(int i = 0; i < length; i++)
+            {
+                var index = length - 1 - i;
+                var current = digits[index];
+                digits[index] = (current + 1) % 10; 
+
+                if (current + 1 <= 9)
+                {                    
+                    return digits;
+                }
+                
+                // continue 
+            }
+
+            var output = new int[length + 1];
+            output[0] = 1;
+
+            Array.Copy(digits, 0, output, 1, length);
+            
+            return output;
+        }
+    }
+}
---
+++ source code/Leetcode 35 Search insert position - binary search.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_35_Search_insert_position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// input array is sorted in ascending order
+        /// find index for the target, if it is not in the array, then return next larger one's index
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int SearchInsert(int[] nums, int target)
+        {
+            if (nums == null || nums.Length == 0)
+                return -1;
+
+            var length = nums.Length;
+            var start = 0;
+            var end = length - 1;
+
+            var smallestIndex = length; 
+            while (start <= end)
+            {
+                var middle = start + (end - start) / 2;
+                var middleValue = nums[middle];
+
+                if (middleValue == target)
+                {
+                    return middle;
+                }
+
+                if (middleValue > target)
+                {
+                    smallestIndex = middle;
+                    end = middle - 1; 
+                }
+                else
+                {
+                    start = middle + 1; 
+                }
+            }
+
+            return smallestIndex;
+        }
+    }
+}
