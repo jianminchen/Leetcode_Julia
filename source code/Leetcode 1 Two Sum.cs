12a9d82da1502a0552c176fba7093f0c7eb40d8c
Jianmin Chen <jianminchen.fl@gmail.com>
2018-08-29 14:18:14 -0700

    check in

---
+++ source code/Leetcode 1 Two Sum.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_1_Two_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// the array is not sorted. To avoid using time complexity more than linear, 
+        /// a hashset can be used to lower down the time complexity to optimal, linear time
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] TwoSum(int[] nums, int target)
+        {
+            if(nums == null || nums.Length < 2)
+                return new int[0];
+
+            var visited = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var current = nums[i];
+                var search = target - current;
+
+                if (visited.ContainsKey(search))
+                {
+                    return new int[] { visited[search], i };
+                }
+                else
+                {
+                    if (!visited.ContainsKey(current))
+                    {
+                        visited.Add(current, i); 
+                    }
+                }
+            }
+
+            return new int[0];
+        }
+    }
+}
