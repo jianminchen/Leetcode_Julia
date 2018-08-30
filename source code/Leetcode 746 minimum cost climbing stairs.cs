d06e82f4199998c8d568a5d115a012abfb9ee2ca
Jianmin Chen <jianminchen.fl@gmail.com>
2018-08-30 11:34:51 -0700

    check in

---
+++ source code/Leetcode 746 minimum cost climbing stairs.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_746_minimum_cost_climbing_stairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// using dynamic programming method to solve the problem
+        /// </summary>
+        /// <param name="cost"></param>
+        /// <returns></returns>
+        public int MinCostClimbingStairs(int[] cost)
+        {
+            if (cost == null)
+                return 0;
+            
+            var length = cost.Length;
+            if (length == 1)
+            {
+                return 0;
+            }
+
+            var dp = new int[length];            
+            
+            for (int i = 0; i < length; i++)
+            {
+                if (i <= 1)
+                {
+                    dp[i] = cost[i];
+                }
+                else
+                {
+                    dp[i] = cost[i] + Math.Min(dp[i - 1], dp[i - 2]);
+                }
+            }
+
+            return Math.Min(dp[length - 1], dp[length - 2]);
+        }
+    }
+}
