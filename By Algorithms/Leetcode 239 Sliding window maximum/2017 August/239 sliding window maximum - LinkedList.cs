079d6767a4b09f615c1a9cf487e5d42de75ab8ad
Jianmin Chen <jianminchen.fl@gmail.com>
2018-09-13 10:46:28 -0700

    check in

---
+++ By Algorithms/Leetcode 239 Sliding window maximum/2017 August/239 sliding window maximum - LinkedList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _239_sliding_window_maximum___August_2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int[] MaxSlidingWindow(int[] nums, int k)
+        {
+            if (k == 0)
+            {
+                return nums; // julia: base case is added to pass online judge. 
+            }
+
+            int length = nums.Length;
+            int numberOfSlidingWindow = length - k + 1;
+
+            var maximums = new int[numberOfSlidingWindow];
+
+            var linkedList = new LinkedList<int>();  // LinkedList acts like deque
+
+            for (int index = 0; index < length; index++)
+            {
+                var current = nums[index];
+
+                // Remove the first number in the queue if it falls out of the sliding window
+                if (linkedList.Count > 0 && linkedList.First.Value + k <= index)
+                {
+                    linkedList.RemoveFirst();
+                }
+
+                // Visit the current number, remove from the tail of the
+                // queue indices if the value is smaller than the current number.
+                while (linkedList.Count > 0 && nums[linkedList.Last.Value] <= current)
+                {
+                    linkedList.RemoveLast();
+                }
+
+                linkedList.AddLast(index);
+
+                // Set the max value in the window (always the top number in the queue)                
+                int maximumsIndex = index + 1 - k;
+                if (maximumsIndex >= 0)
+                {
+                    maximums[maximumsIndex] = nums[linkedList.First.Value];
+                }
+            }
+
+            return maximums;
+        }
+    }
+}
---
+++ By Algorithms/Leetcode 239 Sliding window maximum/239 Sliding Window maximum - using SortedSet.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _239_Sliding_window_maximum___Sortedset___logK_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// the solution does not have optimal time complexity
+        /// 
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public int[] MaxSlidingWindow(int[] nums, int k)
+        {
+            if (k < 1 || nums.Length == 0)
+            {
+                return new int[0];
+            }
+
+            var result = new int[nums.Length - k + 1];
+
+            var map = new Dictionary<int, int>(nums.Length);
+
+            var binarySearchTree = new SortedSet<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var visit = nums[i];
+
+                binarySearchTree.Add(visit);   // O(LogK)
+
+                map[visit] = i;       // O(1), duplicate number will be updated with latest index
+
+                if (i < k - 1)
+                {
+                    continue;
+                }
+
+                if (i >= k && map[nums[i - k]] == (i - k))  // O(1)
+                {
+                    var kStepAway = nums[i - k];
+
+                    binarySearchTree.Remove(kStepAway);    // O(logK)
+
+                    map.Remove(kStepAway);        // O(1)
+                }
+
+                result[i - k + 1] = binarySearchTree.Max;    // O(1)
+            }
+
+            return result;
+        }
+    }
+}
