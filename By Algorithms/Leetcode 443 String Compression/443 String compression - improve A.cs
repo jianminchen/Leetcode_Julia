438c1e280a5413932ab77a8056e64705abe62a31
Jianmin Chen <jianminchen.fl@gmail.com>
2018-09-21 12:50:54 -0700

    check in

---
+++ By Algorithms/Leetcode 443 String Compression/443 String compression - improve A.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _443_string_compression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chars = "x77222f'4}".ToCharArray();
+
+            var result = Compress(chars);
+        }
+
+        /// <summary>
+        /// compress the string
+        /// </summary>
+        /// <param name="chars"></param>
+        /// <returns></returns>
+        public static int Compress(char[] chars)
+        {
+            if (chars == null || chars.Length == 0)
+                return 0;
+
+            var length = chars.Length;                      
+            char previous = chars[0];
+
+            int count = 0;  
+
+            int index = 0;
+            int totalUsed = 0; 
+            while(index < length)
+            {
+                var current = chars[index];
+                
+                while(index < length && chars[index] == current )
+                {
+                    count++; 
+                    index++;
+                }
+
+                if (count == 1)
+                {
+                    chars[totalUsed] = current;
+                    totalUsed++;
+                }
+                else
+                {
+                    chars[totalUsed++] = current;
+                    var numbers = count.ToString();
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        chars[totalUsed++] = numbers[i];
+                    }
+                }
+
+                count = 0;                 
+            }
+
+            return index;
+        }
+    }
+}
---
+++ source code/383 Ransom note.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _383_ransom_note
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// ransomNote should be saved in dictionary<char, int> 
+        /// magazine should also be saved in dictionary<char, int> 
+        /// </summary>
+        /// <param name="ransomNote"></param>
+        /// <param name="magazine"></param>
+        /// <returns></returns>
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            if (ransomNote == null || magazine == null)
+                return false;
+
+            var ransomDict = saveToDict(ransomNote);
+            var magazineDict = saveToDict(magazine);
+
+            foreach (var pair in ransomDict)
+            {
+                var key = pair.Key;
+                var value = pair.Value;
+
+                if (!magazineDict.ContainsKey(key) || magazineDict[key] < value)
+                    return false; 
+            }
+
+            return true; 
+        }
+
+        private static Dictionary<char, int> saveToDict(string words)
+        {
+            var dict = new Dictionary<char, int>();
+
+            foreach (var item in words)
+            {
+                if (!dict.ContainsKey(item))
+                {
+                    dict.Add(item, 0);
+                }
+
+                dict[item]++; 
+            }
+
+            return dict; 
+        }
+    }
+}
