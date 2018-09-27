597e75759f74308c5fe80edbbf8d81d2ae2f6cb5
Jianmin Chen <jianminchen.fl@gmail.com>
2018-09-27 11:32:37 -0700

    check in

---
+++ By Algorithms/Leetcode 720 Find longest word in dictionary/720 longest word in dictionary - using sorting and hashset.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _720_longest_word_in_dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// study code: 
+        /// 
+        /// </summary>
+        /// <param name="words"></param>
+        /// <returns></returns>
+        public string LongestWord(string[] words) {
+            if(words == null)
+                return null;
+
+            Array.Sort(words); 
+
+            // word with prefixes in the dictionary
+            var visitedPrefix = new HashSet<string>(); 
+
+            string maximumWord = "";
+
+            foreach(var word in words)
+            {
+                var length = word.Length;
+
+                // think carefully about this reasoning using w, wo, wor, word
+                if(length == 1 || visitedPrefix.Contains(word.Substring(0, length - 1)))
+                {
+                    // keep lexico order - apple will be checked before apply
+                    maximumWord = word.Length > maximumWord.Length? word : maximumWord; 
+                    visitedPrefix.Add(word); 
+                }
+            }
+
+            return maximumWord;
+        }
+    }
+}
