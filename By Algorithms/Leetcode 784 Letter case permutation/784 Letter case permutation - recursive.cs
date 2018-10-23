8f601654f24f5bf0b16a38a4aea89eff6cdaebf7
Jianmin Chen <jianminchen.fl@gmail.com>
2018-10-23 12:31:27 -0700

    check in

---
+++ By Algorithms/Leetcode 784 Letter case permutation/784 Letter case permutation - recursive.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _784_Letter_case_permutation___recursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public IList<string> LetterCasePermutation(string source)
+        {
+            if (source == null)
+            {
+                return new List<string>();
+            }
+
+            var list = new List<string>();
+
+            iterateString(source, list, "", 0);
+
+            return list;
+        }
+
+        /// <summary>
+        /// recursive solution
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="options"></param>
+        /// <param name="prefix"></param>
+        /// <param name="index"></param>
+        private static void iterateString(string source, IList<string> options, string prefix, int index)
+        {
+            if (index == source.Length)
+            {
+                options.Add(prefix);
+                return;
+            }
+
+            var current = source[index];
+            var lowercaseLetter = current >= 'a' && current <= 'z';
+            var uppercaseLetter = current >= 'A' && current <= 'Z';
+
+            if (lowercaseLetter || uppercaseLetter)
+            {
+                var next = prefix + current.ToString();
+                char nextAlternateCase = (char)('A' + current - 'a');
+                if (uppercaseLetter)
+                {
+                    nextAlternateCase = (char)('a' + current - 'A');
+                }
+
+                iterateString(source, options, next, index + 1);
+                iterateString(source, options, prefix + nextAlternateCase.ToString(), index + 1);
+            }
+            else
+            {
+                var next = prefix + source[index].ToString();
+                iterateString(source, options, next, index + 1);
+            }
+        }
+    }
+}
---
+++ By Algorithms/Leetcode 784 Letter case permutation/784 Letter case permutation - using stack.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _784_letter_caser_permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public IList<string> LetterCasePermutation(string source)
+        {
+            var list = new List<string>();
+            if (source == null || source.Length == 0)
+            {
+                list.Add("");
+                return list;
+            }
+
+            var length = source.Length;
+            var stack = new Stack<string>();
+
+            stack.Push(source[0].ToString());
+            while (stack.Count > 0)
+            {
+                var prefix = stack.Pop();
+                var length1 = prefix.Length;
+
+                var lastChar = prefix[length1 - 1];
+
+                var lowerCase = lastChar >= 'a' && lastChar <= 'z';
+                var upperCase = lastChar >= 'A' && lastChar <= 'Z';
+                var isLastChar = length1 == length;
+
+                if (lowerCase || upperCase)
+                {
+                    // asumme that it is upper case
+                    var char1 = (char)('a' + lastChar - 'A');
+                    if (lowerCase)
+                    {
+                        char1 = (char)('A' + lastChar - 'a');
+                    }
+
+                    var alternate = prefix.Substring(0, length1 - 1) + char1.ToString();
+                    if (isLastChar)
+                    {
+                        list.Add(alternate);
+                        list.Add(prefix);
+                    }
+                    else
+                    {
+                        var next = source[length1].ToString();
+                        stack.Push(alternate + next);
+                        stack.Push(prefix + next);
+                    }
+                }
+                else
+                {
+                    if (isLastChar)
+                    {
+                        list.Add(prefix);
+                    }
+                    else
+                    {
+                        var next = source[length1].ToString();
+                        stack.Push(prefix + next);
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
