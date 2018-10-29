580e235280f9c23e94a13acb0cba3869bef52ade
Jianmin Chen <jianminchen.fl@gmail.com>
2018-10-29 11:59:59 -0700

    check in

---
+++ source code/929 unique email address.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _929_unique_email_address
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int NumUniqueEmails(string[] emails)
+        {
+            if (emails == null || emails.Length == 0)
+                return 0;
+
+            var hashset = new HashSet<string>();
+            foreach (var email in emails)
+            {
+                //need to figure out email address
+                var split1 = email.Split('@');
+                var domainName = split1[1];
+                var name = split1[0];
+                var userName = name.Split('+');
+                var withDot = userName[0];
+                var splitWithDot = withDot.Split('.');
+                var address = string.Join("", splitWithDot);
+                hashset.Add(address + "@" + domainName);
+            }
+
+            return hashset.Count;
+        }
+    }
+}
---
+++ source code/930 Binary subarrays with sum - written for contest 108.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _930_Binary_subarrays_with_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// the code was written in the weekly contest 108. I did submit five time until the code 
+        /// passed online judge. 
+        /// Here is my discuss link:
+        /// https://leetcode.com/problems/binary-subarrays-with-sum/discuss/186713/C-it-took-me-five-submissions-in-the-contest-one-hour
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int NumSubarraysWithSum(int[] numbers, int target)
+        {
+            if (numbers == null || target < 0)
+                return 0;
+
+            var sum = numbers.Sum();
+            if (target > sum)
+                return 0;
+
+            int length = numbers.Length;
+            var onePositions = new int[sum];
+            if (sum == 0)
+            {
+                return length * (length + 1) / 2;
+            }
+
+            int index = 0;
+
+            var diff = 0;
+            for (int i = 0; i < length; i++)
+            {
+                if (numbers[i] == 1)
+                {
+                    onePositions[index] = i;
+
+                    if (index == 0)
+                    {
+                        diff += i * (i + 1) / 2;
+                    }
+                    else
+                    {
+                        int no = (i - onePositions[index - 1] - 1);
+                        diff += no * (no + 1) / 2;
+                    }
+
+                    index++;
+                }
+            }
+
+            int no2 = length - onePositions[sum - 1] - 1;
+            diff += no2 * (no2 + 1) / 2;
+
+            if (target == 0)
+            {
+                return diff;
+            }
+
+            int totalCount = 0;
+            // need to work on the counting
+            for (int start = 0; start >= 0 && (start + target - 1) < sum; start++)
+            {
+                int end = start + target - 1;
+
+                var startIndex = onePositions[start]; // index out of range
+                var endIndex = onePositions[end];
+
+                int variation1 = 0;
+                if (start == 0)
+                {
+                    variation1 = startIndex + 1;
+                }
+                else
+                {
+                    variation1 = startIndex - onePositions[start - 1];
+                }
+
+                int variation2 = 0;
+                if (end == sum - 1)
+                {
+                    variation2 = length - endIndex;
+                }
+                else
+                {
+                    variation2 = onePositions[end + 1] - endIndex;
+                }
+
+                totalCount += variation1 * variation2;
+            }
+
+            return totalCount;
+        }
+    }
+}
