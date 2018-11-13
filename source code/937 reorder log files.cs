f93c56ac0a416b2c95ad8f411a1a1efa646b4a45
Jianmin Chen <jianminchen.fl@gmail.com>
2018-11-13 10:51:06 -0800

    check in

---
+++ source code/937 reorder log files.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _937_Reorder_log_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public string[] ReorderLogFiles(string[] logs)
+        {
+            if (logs == null || logs.Length == 0)
+            {
+                return new string[0];
+            }
+
+            var digits = new List<string>();
+            var dict = new Dictionary<string, string>();
+
+            var result = new List<string>();
+
+            foreach (var log in logs)
+            {
+                var split = log.Split(' ');
+                var firstWord = split[0];
+                char firstChar = split[1][0];
+                var isLetter = firstChar - 'a' >= 0 && 'z' - firstChar >= 0;
+                if (isLetter)
+                {
+                    dict.Add(log, log.Substring(firstWord.Length));
+                }
+                else
+                {
+                    digits.Add(log);
+                }
+            }
+
+            var myList = dict.ToList();
+
+            myList.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+
+            foreach (var pair in myList)
+            {
+                result.Add(pair.Key);
+            }
+
+            foreach (var item in digits)
+            {
+                result.Add(item);
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ source code/938 Range Sum of BST.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _938_Range_Sum_of_BST
+{
+    public class TreeNode {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int x) { val = x; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int RangeSumBST(TreeNode root, int L, int R)
+        {
+            if (root == null)
+                return 0;
+
+            var value = root.val;
+            if (value >= L && value <= R)
+            {
+                return value + RangeSumBST(root.left, L, R) + RangeSumBST(root.right, L, R);
+            }
+            else if (value > R)
+            {
+                return RangeSumBST(root.left, L, R);
+            }
+            else
+                return RangeSumBST(root.right, L, R);
+        }
+    }
+}
