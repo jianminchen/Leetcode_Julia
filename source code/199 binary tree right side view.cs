fa4ed545f690fb0c9b23edf43bf6f952f1ef713d
Jianmin Chen <jianminchen.fl@gmail.com>
2018-11-19 16:13:52 -0800

    check in

---
+++ source code/199 binary tree right side view.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _199_binary_tree_right_side_view
+{
+     public class TreeNode {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int x) { val = x; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// level by level order traversal, record the last node in each level.
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public IList<int> RightSideView(TreeNode root)
+        {
+            if (root == null)
+                return new List<int>();
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            var lastNodes = new List<int>();
+
+            while (queue.Count > 0)
+            {
+                var levelCount = queue.Count;
+                var lastNode = new TreeNode(-1);
+                for (int i = 0; i < levelCount; i++)
+                {
+                    var node = queue.Dequeue();
+                    if (i == levelCount - 1)
+                    {
+                        lastNode = node;
+                    }
+
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+
+                lastNodes.Add(lastNode.val); 
+            }
+
+            return lastNodes;
+        }
+    }
+}
---
+++ source code/96 unique BST.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _96_unique_BST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// source code is based on the discuss
+        /// https://leetcode.com/problems/unique-binary-search-trees/discuss/31666/DP-Solution-in-6-lines-with-explanation.-F(i-n)-G(i-1)-*-G(n-i)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int NumTrees(int n)
+        {
+            if (n < 0)
+            {
+                return 0;
+            }
+
+            var dp = new int[n + 1];
+            dp[0] = 1; 
+            dp[1] = 1;
+           
+            for (int i = 2; i < n + 1; i++)
+            {
+                dp[i] = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    dp[i] += dp[j] * dp[i - 1 - j];
+                }
+            }
+
+            return dp[n];
+        }        
+    }
+}
