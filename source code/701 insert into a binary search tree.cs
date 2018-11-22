ee067d4cc97bbb8c649c3109e4e37f0fd7b306ef
Jianmin Chen <jianminchen.fl@gmail.com>
2018-11-22 11:08:57 -0800

    check in

---
+++ source code/701 insert into a binary search tree.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _701_insert_into_a_binary_search_tree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public TreeNode InsertIntoBST(TreeNode root, int val)
+        {
+            if (root == null)
+                return new TreeNode(val);
+
+            var value = root.val;
+            if (value > val)
+            {
+                root.left = InsertIntoBST(root.left, val);
+            }
+            else
+            {
+                root.right = InsertIntoBST(root.right, val);
+            }
+
+            return root; 
+        }
+    }
+}
