5ca63e5d146df06e5be8620e7b19f70859bea8d3
Jianmin Chen <jianminchen.fl@gmail.com>
2018-11-22 11:46:22 -0800

    check in

---
+++ source code/814 binary tree pruning.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _814_binary_tree_pruning
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public TreeNode PruneTree(TreeNode root)
+        {
+            if (root == null)
+                return root;
+
+            var value = root.val;
+            var isZero = value == 0;
+            if (isZero && root.left == null && root.right == null)
+                return null;
+
+            var rootLeft = PruneTree(root.left);
+            var rootRight = PruneTree(root.right);
+
+            if (isZero && rootLeft == null && rootRight == null)
+                return null;
+            root.left  = rootLeft;
+            root.right = rootRight;
+
+            return root; 
+        }
+    }
+}
