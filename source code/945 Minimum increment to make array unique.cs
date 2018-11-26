2a55c4af4d59e2e0726d0d44ec8ae75f5df6a9cb
Jianmin Chen <jianminchen.fl@gmail.com>
2018-11-26 11:30:07 -0800

    check in

---
+++ source code/945 Minimum increment to make array unique.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _945_Minimum_increment_to_make_array_unique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int MinIncrementForUnique(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+                return 0;
+
+            Array.Sort(numbers);
+
+            var length = numbers.Length;
+
+            var sum = numbers.Sum();
+
+            var newNumber = new int[length];
+            newNumber[0] = numbers[0];
+
+            for (int i = 1; i < length; i++)
+            {
+                var current = numbers[i];
+
+                newNumber[i] = Math.Max(current, newNumber[i - 1] + 1);
+            }
+
+            return newNumber.Sum() - sum;
+        }
+    }
+}
---
+++ source code/946 Validate stack sequences.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _946_validate_statck_sequences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public bool ValidateStackSequences(int[] pushed, int[] popped)
+        {
+            if (pushed == null || popped == null)
+                return false;
+            if (pushed.Length == 0 && popped.Length == 0)
+                return true;
+            if (pushed.Length != popped.Length)
+                return false;
+
+            var length = pushed.Length;
+
+            var pushIndex = 0;
+            var popIndex = 0;
+            var stack = new Stack<int>();
+            var hashset = new HashSet<int>();
+            var removed = new HashSet<int>();
+
+            while (pushIndex < length || popIndex < length)
+            {
+                var pop = popped[popIndex];
+
+                if (removed.Contains(pop))
+                    return false;
+
+                if (stack.Count > 0 && stack.Peek() == pop)
+                {
+                    stack.Pop();
+                    popIndex++;
+                    hashset.Remove(pop);
+                    continue;
+                }
+
+                if (hashset.Contains(pop))
+                {
+                    while (stack.Count > 0)
+                    {
+                        var number = stack.Pop();
+                        hashset.Remove(number);
+                        removed.Add(number);
+                        if (number == pop)
+                        {
+                            popIndex++;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    if (pushIndex < length)
+                    {
+                        var visit = pushed[pushIndex];
+                        stack.Push(visit);
+                        pushIndex++;
+                        hashset.Add(visit);
+                    }
+                }
+            }
+
+            return pushIndex == length && popIndex == length;
+        }
+    }
+}
