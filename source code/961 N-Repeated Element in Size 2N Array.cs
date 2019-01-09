e6876baca20db01e0966955020a7de4a01e4d417
Jianmin Chen <jianminchen.fl@gmail.com>
2019-01-09 11:23:56 -0800

    check in

---
+++ source code/961 N-Repeated Element in Size 2N Array.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _961_N_repeated_element_in_the_array
+{
+    /// <summary>
+    /// N-Repeated Element in Size 2N Array
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int RepeatedNTimes(int[] numbers)
+        {
+            var length = numbers.Length;
+
+            var hashset = new HashSet<int>();
+
+            for (int i = 0; i < length; i++)
+            {
+                var current = numbers[i];
+                if (hashset.Contains(current))
+                {
+                    return current;
+
+                }
+
+                hashset.Add(current);
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ source code/965 Univalued Binary tree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _965_Univalued_Binary_tree
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public bool IsUnivalTree(TreeNode root)
+        {
+            if (root == null)
+                return true;
+
+            if (root.left == root.right && root.left == null)
+                return true;
+
+            return (root.left == null || (root.val == root.left.val && IsUnivalTree(root.left))) &&
+                   (root.right == null || (root.val == root.right.val && IsUnivalTree(root.right)));
+        }
+    }
+}
