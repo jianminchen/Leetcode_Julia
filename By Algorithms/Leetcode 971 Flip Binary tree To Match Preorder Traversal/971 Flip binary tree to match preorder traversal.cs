e7668c0470eca006ffd2de1513b703eebedb41b9
Jianmin Chen <jianminchen.fl@gmail.com>
2019-01-09 12:00:36 -0800

    check in

---
+++ By Algorithms/Leetcode 971 Flip Binary tree To Match Preorder Traversal/971 Flip binary tree to match preorder traversal.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _971_Flip_binary_tree_to_match_preorder_traversal
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public IList<int> FlipMatchVoyage(TreeNode root, int[] voyage)
+        {
+            if (root == null)
+                return new List<int>();
+
+            var succeeded = true;
+
+            if (root.val != voyage[0])
+                return new List<int>(new int[] { -1 });
+            var list = new List<int>();
+
+            preorderTraversal(root, voyage, 1, ref succeeded, list);
+            if (!succeeded)
+                return new List<int>(new int[] { -1 });
+
+            return list;
+        }
+
+        /// <summary>
+        /// need to consider left child is null
+        /// flip left and right node
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="voyage"></param>
+        /// <param name="index"></param>
+        /// <param name="succeeded"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        private static int preorderTraversal(TreeNode root, int[] voyage, int index, ref bool succeeded, List<int> result)
+        {
+            if (index >= voyage.Length || succeeded == false)
+            {
+                return voyage.Length + 1;
+            }
+
+            var current = voyage[index];
+
+            var lastIndex = index;
+
+            if (root.left != null)
+            {
+                if (root.left.val != current)
+                {
+                    if (root.right == null || root.right.val != current)
+                    {
+                        succeeded = false;
+                        return voyage.Length;
+                    }
+                    else
+                    {
+                        // flip
+                        var tmp = root.left;
+
+                        root.left = root.right;
+                        root.right = tmp;
+
+                        result.Add(root.val);
+                    }
+                }
+
+                lastIndex = preorderTraversal(root.left, voyage, index + 1, ref succeeded, result);
+            }
+
+            if (root.right != null)
+            {
+                if (lastIndex >= voyage.Length || root.right.val != voyage[lastIndex])
+                {
+                    succeeded = false;
+                    return voyage.Length;
+                }
+
+                lastIndex = preorderTraversal(root.right, voyage, lastIndex + 1, ref succeeded, result);
+            }
+
+            return lastIndex;
+        }
+    }
+}
---
+++ source code/971 Flip binary tree to match preorder traversal - elegant solution.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _971_Flip_binary_tree_to_match_preorder_traversal_2
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static IList<int> FlipMatchVoyage(TreeNode root, int[] voyage)
+        {
+            if (root == null)
+                return new List<int>();
+
+            var list = new List<int>();
+            int index = 0; 
+
+            var result = preorderTraversal(root, voyage, ref index, list);
+            if(result)
+            {
+                return list; 
+            }
+            else 
+            {
+                return new int[]{-1}.ToList<int>();
+            }
+        }
+
+        /// <summary>
+        /// Keep my work to minimum
+        /// Ask myself what to work on. 
+        /// 1. Check root node's value, return false in my code
+        /// 2. Iterate to next position
+        /// 3. Swap left and right node
+        /// 4. if step 3 is true, add root node's value to the list
+        /// 5. Recursive check all subtrees. 
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="voyage"></param>
+        /// <param name="index"></param>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        private static bool preorderTraversal(TreeNode root, int[] voyage, ref int index, List<int> list)
+        {
+            if (root == null)
+                return true;
+
+            // root node's value should equal to the value in the array
+            if (root.val != voyage[index])
+            {
+                return false;
+            }
+
+            index++;
+
+            if (root.left != null && root.left.val != voyage[index])
+            {
+                // swap left and right child of root
+                var tmp = root.left;
+                root.left = root.right;
+                root.right = tmp;
+                list.Add(root.val); 
+            }
+
+            return preorderTraversal(root.left, voyage, ref index, list) && 
+                   preorderTraversal(root.right,voyage, ref index, list);
+        }
+    }
+}
