915212a1653054f4b186a5b3db53fecebd2cddb9
Jianmin Chen <jianminchen.fl@gmail.com>
2019-01-11 11:10:19 -0800

    check in

---
+++ By Algorithms/Leetcode 37 Sudoku solver/Sudoku solver - Hashset idea.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _37_Sudoku_solver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// code review based on 
+        /// http://anothercasualcoder.blogspot.com/2019/01/sudoku-solver-hard-leetcode-problem.html
+        /// A lot of good ideas are included in this implementation. 
+        /// I can enumerate them one by one. 
+        /// 1. Preprocess all rows and columns and quadrants into data structure, O(1) to look up
+        /// if the digit is available for next empty slot;
+        /// 2. index variable from 0 to 80 is used to track the progress of depth first search, 
+        /// help to identify base case;
+        /// 3. Step 1 is also very efficient since every element with digit values in the board will 
+        /// be added once at the beginning. 
+        ///  
+        /// </summary>
+        /// <param name="board"></param>
+        public static void SolveSudoku(char[][] board)
+        {
+            var rowDigits = new HashSet<int>[9];
+            var columnDigits = new HashSet<int>[9];
+            var quadrantDigits = new HashSet<int>[9];
+
+            for (int i = 0; i < 9; i++)
+            {
+                rowDigits[i]      = new HashSet<int>();
+                columnDigits[i]   = new HashSet<int>();
+                quadrantDigits[i] = new HashSet<int>();
+            }
+
+            //Add all current values to the hash set
+            int index = 0;
+            for (int r = 0; r < 9; r++)
+            {
+                for (int c = 0; c < 9; c++)
+                {
+                    if (board[r][c] != '.')
+                    {
+                        AddNumber(index, (int)(board[r][c] - '0'), rowDigits, columnDigits, quadrantDigits);
+                    }
+
+                    index++;
+                }
+            }
+
+            SolveSudoku(board, 0, rowDigits, columnDigits, quadrantDigits);
+        }
+
+        private static bool SolveSudoku(char[][] board,
+               int index,
+               HashSet<int>[] rowDigits,
+               HashSet<int>[] columnDigits,
+               HashSet<int>[] quadrantDigits)
+        {
+            if (index == 81) return true;
+
+            int r = index / 9;
+            int c = index % 9;
+            if (board[r][c] != '.')
+            {
+                return SolveSudoku(board, index + 1, rowDigits, columnDigits, quadrantDigits);
+            }
+            else
+            {
+                for (int n = 1; n <= 9; n++)
+                {
+                    if (AddNumber(index, n, rowDigits, columnDigits, quadrantDigits))
+                    {
+                        board[r][c] = (char)(n + '0');
+
+                        if (SolveSudoku(board, index + 1, rowDigits, columnDigits, quadrantDigits))
+                        {
+                            return true;
+                        }
+
+                        // backtracking 
+                        board[r][c] = '.';
+                        RemoveNumber(index, n, rowDigits, columnDigits, quadrantDigits);
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// The row and column should not include same digit more than once; quadrant as well.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="number"></param>
+        /// <param name="rows"></param>
+        /// <param name="cols"></param>
+        /// <param name="quadrants"></param>
+        /// <returns></returns>
+        private static bool AddNumber(int index,
+              int number,
+              HashSet<int>[] rows,
+              HashSet<int>[] cols,
+              HashSet<int>[] quadrants)
+        {
+            int r = index / 9;
+            int c = index % 9;
+            int q = (index / 27) * 3 + (index % 9) / 3;
+
+            if (!rows[r].Contains(number) && 
+                !cols[c].Contains(number) && 
+                !quadrants[q].Contains(number))
+            {
+                rows[r].Add(number);
+                cols[c].Add(number);
+
+                quadrants[q].Add(number);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private static void RemoveNumber(int index,
+               int number,
+               HashSet<int>[] rows,
+               HashSet<int>[] cols,
+               HashSet<int>[] quadrants)
+        {
+            int r = index / 9;
+            int c = index % 9;
+            int q = (index / 27) * 3 + (index % 9) / 3;
+
+            rows[r].Remove(number);
+            cols[c].Remove(number);
+            quadrants[q].Remove(number);
+        }
+    }
+}
---
+++ By Algorithms/Leetcode 503 Next Great Element  II/503 next great element II.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _503_next_great_element_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = NextGreaterElements(new int[]{1, 5, 3, 6, 8}); 
+        }
+
+        /// <summary>
+        /// Time complexity: O(N)
+        /// Step 1: Get next great element from the right side of the array first;
+        ///  idea is to reverse the array, and then use stack to keep monotonic decreasing order. 
+        /// Step 2: Reverse the result array
+        /// Step 3: If elment in the array does not have greater element in the right side, then
+        ///  work on the search from the left side the array. 
+        ///  Only one iteration from left to right side is needed. Why? 
+        ///  Iterate the array from end to start and the values of those missing ones should be increasing
+        ///  order. 
+        /// test case 1:
+        /// [1, 2, 3]
+        /// -> reverse [3, 2, 1] -> ok
+        /// test case 2:
+        /// [4, 2, 3]
+        /// -> reverse [3, 2, 4] 
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public static int[] NextGreaterElements(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+                return new int[0];
+
+            var length = numbers.Length;
+            var copy = new int[length];
+
+            Array.Copy(numbers, copy, length);
+
+            Array.Reverse(numbers); // [3, 2,1]            
+
+            var nextGreater      = new int[length];
+            var nextGreaterFound = new bool[length];
+
+            for (int i = 0; i < length; i++)
+                nextGreater[i] = -1;
+
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < length; i++)
+            {
+                if (i == 0)
+                {
+                    stack.Push(i); // 0
+                    continue; 
+                }
+
+                var current = numbers[i]; // 2
+                var top = stack.Peek();                 
+
+                while (stack.Count > 0 && numbers[stack.Peek()] <= current)
+                {                                        
+                    stack.Pop();                    
+                }
+
+                if (stack.Count > 0)
+                {
+                    nextGreater[i] = numbers[stack.Peek()];
+                    nextGreaterFound[i] = true;
+                }
+
+                stack.Push(i); 
+            }
+            
+            Array.Reverse(nextGreater);
+            Array.Reverse(nextGreaterFound);
+
+            // iterate from left to right - O(N) - get maximum from left side
+            int start = 0; 
+            for (int i = 0; i < length; i++)
+            {               
+                var reverse = length - 1 - i;
+                if (nextGreaterFound[reverse])
+                    continue;
+
+                while (start < reverse)
+                {
+                    if (start < reverse && copy[start] > copy[reverse])
+                    {
+                        nextGreater[reverse] = copy[start];                        
+                        break;
+                    }
+
+                    start++;
+                }                
+            }
+
+            return nextGreater;
+        }
+    }
+}
