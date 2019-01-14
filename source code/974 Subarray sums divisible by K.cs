f515422458465acd06c13c115cfd7ba2008db246
Jianmin Chen <jianminchen.fl@gmail.com>
2019-01-14 11:05:20 -0800

    check in

---
+++ source code/974 Subarray sums divisible by K.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _974_Subarray_sums_divisible_by_K
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int SubarraysDivByK(int[] numbers, int K)
+        {
+            var countResidue = new int[K];
+            int count = 0;
+            int prefixSum = 0;
+            countResidue[0] = 1;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var current = numbers[i];
+                prefixSum += current;
+                var residue = prefixSum % K;
+                if (residue < 0)
+                {
+                    residue = K + residue;
+                }
+
+                count += countResidue[residue];
+                countResidue[residue]++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ source code/976 Largest perimeter triangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _976_Largest_perimeter_triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int LargestPerimeter(int[] numbers)
+        {
+            Array.Sort(numbers);
+            var length = numbers.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                var lastOne = length - 1 - i;
+                var nextToLast = lastOne - 1;
+                var smallest = lastOne - 2;
+                if (smallest < 0)
+                {
+                    break;
+                }
+
+                if (numbers[nextToLast] + numbers[smallest] > numbers[lastOne])
+                {
+                    return numbers[nextToLast] + numbers[smallest] + numbers[lastOne];
+                }
+            }
+
+            return 0;
+        }
+    }
+}
