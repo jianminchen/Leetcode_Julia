b25ae37076916d48d66b467936458f6efb8f3a31
Jianmin Chen <jianminchen.fl@gmail.com>
2019-02-25 14:17:50 -0800

    check in

---
+++ source code/997 Find the town judge.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _997_Find_the_town_judge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// code written in the weekly contest 125
+        /// </summary>
+        /// <param name="N"></param>
+        /// <param name="trust"></param>
+        /// <returns></returns>
+        public int FindJudge(int N, int[][] trust)
+        {
+            if (N == 1 && (trust == null || trust.Length == 0 || trust[0].Length == 0))
+                return 1;
+
+            if (trust == null || trust.Length == 0)
+                return -1;
+
+            var rows = trust.Length;
+            var columns = trust[0].Length;
+
+            var townJudge = new HashSet<int>[N + 1];
+            for (int i = 1; i < N + 1; i++)
+                townJudge[i] = new HashSet<int>();
+
+            var notTownJudge = new HashSet<int>();
+
+            for (int row = 0; row < rows; row++)
+            {
+                var A = trust[row][0];
+                var B = trust[row][1];
+
+                townJudge[A].Clear();
+
+                notTownJudge.Add(A);
+
+                if (!notTownJudge.Contains(B))
+                {
+                    townJudge[B].Add(A);
+                }
+            }
+
+            for (int i = 1; i < N + 1; i++)
+            {
+                if (townJudge[i].Count == N - 1)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ source code/998 Maximum binary tree II.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _998_Maximum_binary_tree_II
+{
+    public class TreeNode {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int x) { val = x; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public TreeNode InsertIntoMaxTree(TreeNode root, int val)
+        {
+            if (root == null)
+                return new TreeNode(val);
+
+            var current = root.val;
+            if (val > current)
+            {
+                var existing = root.val;
+                var newRoot = new TreeNode(val);
+                newRoot.left = root;
+
+                return newRoot;
+            }
+            else
+            {
+                root.right = InsertIntoMaxTree(root.right, val);
+
+                return root;
+            }
+        }
+    }
+}
