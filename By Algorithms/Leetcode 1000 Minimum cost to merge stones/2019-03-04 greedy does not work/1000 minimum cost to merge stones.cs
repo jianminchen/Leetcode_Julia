b6f76e8fd3c54da5257b40b9fc15552668ba25d5
Jianmin Chen <jianminchen.fl@gmail.com>
2019-03-04 15:17:17 -0800

    check in

---
+++ By Algorithms/Leetcode 1000 Minimum cost to merge stones/2019-03-04 greedy does not work/1000 minimum cost to merge stones.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1000_minimum_cost_to_merge_stones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = MergeStones(new int[]{4, 6, 4, 7, 5}, 2);
+        }
+
+        /// <summary>
+        /// greedy algorithm written by Julia - 2019-03-04
+        /// Fail test case: 
+        /// [69,39,79,78,16,6,36,97,79,27,14,31,4]  2
+        /// output: 2014
+        /// Expected: 1954
+        /// </summary>
+        /// <param name="stones"></param>
+        /// <param name="K"></param>
+        /// <returns></returns>
+        public static int MergeStones(int[] stones, int K)
+        {
+            bool cannotFoundMinimum = false;
+            var number = findMinimumCost(stones, K, ref cannotFoundMinimum);
+            return cannotFoundMinimum ? -1 : number;
+        }
+
+        private static int findMinimumCost(int[] stones, int K, ref bool cannotFoundMinimum)
+        {
+            if (cannotFoundMinimum)
+            {
+                return -1;
+            }
+
+            if (stones == null || stones.Length <= 1)
+                return 0;
+
+            var length = stones.Length;
+
+            if (length < K)
+            {
+                cannotFoundMinimum = true;
+                return -1;
+            }
+
+            var sum = new int[length];
+
+            var prefix = 0;
+            for (int i = 0; i < length; i++)
+            {
+                sum[i] = prefix + stones[i];
+                prefix = sum[i];
+            }
+
+            var startIndexes = new HashSet<int>();            
+            var minKSum = int.MaxValue;
+            for (int i = 0; i + K - 1 < length; i++)
+            {
+                var kSum = sum[i + K - 1];
+                if (i > 0)
+                {
+                    kSum -= sum[i - 1];
+                }
+
+                if (kSum < minKSum)
+                {
+                    startIndexes.Clear();
+                    startIndexes.Add(i);
+                    minKSum = kSum;
+                }
+                else if (kSum == minKSum)
+                {
+                    startIndexes.Add(i);
+                }
+            }
+
+            var minCost = int.MaxValue; 
+            foreach(var minOne in startIndexes)
+            {
+                var subArray = new int[length - K + 1];
+                int count = 0;
+                for (int i = 0; i < minOne; i++)
+                {
+                    subArray[i] = stones[i];
+                    count++;
+                }
+
+                subArray[count++] = minKSum;
+
+                for (int i = minOne + K; i < length; i++)
+                {
+                    subArray[count] = stones[i];
+                    count++;
+                }
+
+                var currentCost = minKSum + findMinimumCost(subArray, K, ref cannotFoundMinimum);
+                minCost = currentCost < minCost? currentCost : minCost;
+            }
+
+            return minCost;
+        }
+    }
+}
--- By Algorithms/Leetcode 687 longest univalue path/ReadMe.md
+++ By Algorithms/Leetcode 687 longest univalue path/ReadMe.md
@@ -7,3 +7,12 @@ Nov. 5, 2018<br>
 I gave the algorithm to an interviewee in the mock interview 10:00 PM in Nov. 4, who likes to apply Google, Facebook, Uber, with five year experience in Microsoft. She wrote the solution and explained to me her idea. The recursive function is designed to work on longest univalue path, the return value is how many nodes in the path. <br>
 
 I made a minor change, and the code can be looked up [here](https://github.com/jianminchen/Leetcode_Julia/blob/master/By%20Algorithms/Leetcode%20687%20longest%20univalue%20path/687%20longest%20univalue%20path%20-%20recursive%20longest%20univalue%20path.cs). <br>
+
+2019-03-04<br>
+
+I chose to ask interviewee to work on the algorithm whenever I have a mock interview. First 30 interviews I learn things from interviewees. I also try very hard every time to have discussion with an interviewee. 
+
+The challenge question to ask myself is when I should master the algorithm. 
+
+
+
