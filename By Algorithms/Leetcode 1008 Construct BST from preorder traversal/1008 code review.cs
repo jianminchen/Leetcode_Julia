009212d6914d234bf8baba791ad54309d70b6ecf
Jianmin Chen <jianminchen.fl@gmail.com>
2019-03-11 15:16:20 -0700

    check in

---
+++ By Algorithms/Leetcode 1008 Construct BST from preorder traversal/1008 code review.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_1008
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = BstFromPreorder(new int[] { 8, 5, 1, 7, 10, 12 });
+        }
+
+        /// <summary>
+        /// 2019-03-11
+        /// I like to review the code written in the contest and then make some improvements
+         
+        /// </summary>
+        /// <param name="preorder"></param>
+        /// <returns></returns>
+        public static TreeNode BstFromPreorder(int[] preorder)
+        {
+            return preorderTraversal(preorder, 0, preorder.Length - 1);
+        }
+
+        /// <summary>
+        /// March 11, 2019
+        /// based on the code written in the contest, I like to review the code and come out 
+        /// the idea to improve the performance. 
+        /// </summary>
+        /// <param name="preorder"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        private static TreeNode preorderTraversal(int[] preorder, int start, int end)
+        {
+            if (start >= preorder.Length || start > end)
+            {
+                return null;
+            }
+
+            var rootValue = preorder[start];
+
+            if (start == end)
+            {
+                return new TreeNode(rootValue);
+            }
+
+            var root = new TreeNode(rootValue);
+            var newStart = start + 1;
+            var lastIndex = search(preorder, root.val, start, end);           
+
+            root.left  = preorderTraversal(preorder, newStart,      lastIndex);
+            root.right = preorderTraversal(preorder, lastIndex + 1, end);
+
+            return root;
+        }
+
+        /// <summary>
+        /// design the function to avoid user case -1, not found.         
+        /// </summary>
+        /// <param name="preorder"></param>
+        /// <param name="value"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        private static int search(int[] preorder, int value, int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                var current = preorder[i];
+                if (current <= value && (i == end || preorder[i + 1] > value))
+                {
+                    return i;
+                }
+            }
+
+            return start; 
+        }
+    }
+}
