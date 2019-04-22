de06ba5852f57dc8b8cf8827e67a516c1740e1a4
Jianmin Chen <jianminchen.fl@gmail.com>
2019-04-22 11:02:26 -0700

    check in

---
+++ By Algorithms/Leetcode 402 Remove K digits/402 Brute force.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _402_remove_K_digits_naive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// Leetcode 402 remove K digits
+        /// 4/19/2019
+        /// I learned two lessons. 
+        /// first I should evaluate time complexity; if I choose to use stack then it will much easy to write;
+        /// Second the edge case is not handled time efficent way. 
+        /// </summary>
+        /// <param name="num"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public string RemoveKdigits(string num, int k)
+        {
+            if (k == 0)
+                return num;
+            if (num == null || num.Length == 0)
+                return num;
+
+            var length = num.Length;
+            var chose = new StringBuilder();
+
+            // time complexity is not efficient O(removed * removed) + O(length)
+            int start = 0;
+            int removed = k;
+            while (removed > 0)
+            {
+                // 10200 - k = 1, only choose the first two digits
+                // find the minimum digit from start position
+                // removed + 1 digits
+                // reserve digits for 
+                var range = removed + 1;
+                var minIndex = start;
+                var minValue = 10;
+
+                // edge case: "10", 2
+                var end = start + range;
+                if (end > length)
+                {
+                    if (chose.ToString().Length == 0)
+                        return "0";
+                    else
+                        return chose.ToString();
+                }
+
+                for (int i = start; i < end; i++)
+                {
+                    var current = num[i] - '0';
+                    var isSmaller = current < minValue;
+                    minValue = isSmaller ? current : minValue;
+                    minIndex = isSmaller ? i : minIndex;
+                }
+
+                // edge case: "0200" -> "20"
+                var leadingZero = chose.ToString().CompareTo("0") == 0;
+                if (leadingZero)
+                {
+                    chose.Clear();
+                    chose.Append(num[minIndex]);
+                }
+                else
+                    chose.Append(num[minIndex]);
+
+                // count how many digits removed
+                removed -= minIndex - start;
+                start = minIndex + 1;
+            }
+
+            for (int i = start; i < length; i++)
+            {
+                // edge case: "0200" -> "20"
+                var leadingZero = chose.ToString().CompareTo("0") == 0;
+                if (leadingZero)
+                {
+                    chose.Clear();
+                    chose.Append(num[i]);
+                }
+                else
+                    chose.Append(num[i]);
+            }
+
+            return chose.ToString();
+        }
+    }
+}
---
+++ By Algorithms/Leetcode 402 Remove K digits/402 remove k digits Optimal.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _402_remove_k_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// Leetcode 402 remove K digits
+        /// 4/19/2019
+        /// I learned two lessons. 
+        /// first I should evaluate time complexity; if I choose to use stack then it will much easy to write;
+        /// Second the edge case is not handled time efficent way. 
+        /// </summary>
+        /// <param name="num"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public string RemoveKdigits(string num, int k)
+        {
+            if (k == 0)
+                return num;
+            if (num == null || num.Length == 0)
+                return num;
+
+            var length = num.Length;
+            var chose = new StringBuilder();
+
+            var stack = new Stack<char>();
+
+            for (int i = 0; i < length; i++)
+            {
+                var current = num[i];
+                while (stack.Count > 0 && stack.Peek() > current && k > 0)
+                {
+                    stack.Pop();
+                    k--;
+                }
+
+                stack.Push(current);
+            }
+
+            // edge case: "9", 1
+            while (k > 0 && stack.Count > 0)
+            {
+                stack.Pop();
+                k--;
+            }
+
+            var numbers = stack.ToList();
+
+            var sb = new StringBuilder();
+            var foundNonZero = false;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                var index = numbers.Count - i - 1;
+                var current = numbers[index];
+                if (!foundNonZero && current == '0')
+                {
+                    continue;
+                }
+
+                foundNonZero = true;
+                sb.Append(current);
+            }
+
+            return sb.ToString().Length == 0 ? "0" : sb.ToString();
+        }
+    }
+}
