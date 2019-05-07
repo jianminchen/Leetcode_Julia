7642d51a6d27318a9c933c0d0198a2611d6aaf48
Jianmin Chen <jianminchen.fl@gmail.com>
2019-05-07 10:33:51 -0700

    check in

---
+++ By Algorithms/Leetcode 236 Lowest Common Ancestor/timeout practice/Leetcode 236 using List.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lowest_common_ancestor_235_timeout
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static List<TreeNode> rootToP, rootToQ;
+
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root == null || p == null || q == null)
+                return null;
+
+            rootToP = new List<TreeNode>();
+            rootToQ = new List<TreeNode>();
+
+            var newList = new List<TreeNode>();
+            traversalPreorder(root, root, p, q, newList);
+
+            if (rootToP.Count == 0 || rootToQ.Count == 0)
+            {
+                return null;
+            }
+
+            int index = 0;
+            TreeNode ancestor = null;
+            while (index < rootToP.Count && index < rootToQ.Count)
+            {
+                if (rootToP[index] == rootToQ[index])
+                {
+                    ancestor = rootToP[index];
+                }
+                else
+                    break;
+                //
+                index++;
+            }
+
+            return ancestor;
+        }
+
+        private void traversalPreorder(TreeNode root, TreeNode current, TreeNode p, TreeNode q, List<TreeNode> prefix)
+        {
+            prefix.Add(current);
+
+            if (current == p && current == q)
+            {
+                foreach (var item in prefix)
+                {
+                    rootToP.Add(item);
+                    rootToQ.Add(item);
+                }
+            }
+
+            if (current == p)
+            {
+                foreach (var item in prefix)
+                {
+                    rootToP.Add(item);
+                }
+            }
+
+            if (current == q)
+            {
+                foreach (var item in prefix)
+                {
+                    rootToQ.Add(item);
+                }
+            }
+
+            if (rootToQ.Count > 0 && rootToP.Count > 0)
+                return;
+
+            if (current.left != null)
+            {
+                var newList = new List<TreeNode>(prefix);
+
+                traversalPreorder(root, current.left, p, q, newList);
+            }
+
+            if (current.right != null)
+            {
+                var newList = new List<TreeNode>(prefix);
+
+                traversalPreorder(root, current.right, p, q, newList);
+            }
+        }
+    }
+}
---
+++ By Algorithms/Leetcode 236 Lowest Common Ancestor/timeout practice/Leetcode 236 using string.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lowest_common_ancestor
+{
+    public class TreeNode {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int x) { val = x; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static StringBuilder rootToP, rootToQ;
+
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root == null || p == null || q == null)
+                return null;
+
+            rootToP = new StringBuilder();
+            rootToQ = new StringBuilder();
+
+            var newList = new StringBuilder();
+            newList.Append('0');
+            traversalPreorder(root, root, p, q, newList);
+
+            if (rootToP.ToString().Length == 0 || rootToQ.ToString().Length == 0)
+            {
+                return null;
+            }
+
+            int index = 0;
+            TreeNode ancestor = null;
+            var pStr = rootToP.ToString();
+            var qStr = rootToQ.ToString();
+
+            while (index < rootToP.ToString().Length && index < rootToQ.ToString().Length)
+            {
+                if (pStr[index] != qStr[index])
+                {
+                    break;
+                }
+                
+                var numbers = rootToP.ToString();
+                var current = numbers[index];
+                if (index == 0)
+                {
+                    ancestor = root;
+                }
+                else
+                {
+                    if (current == 0)
+                        ancestor = ancestor.left;
+                    else
+                        ancestor = ancestor.right;
+                }
+                
+                //
+                index++;
+            }
+
+            return ancestor;
+        }
+
+        private void traversalPreorder(TreeNode root, TreeNode current, TreeNode p, TreeNode q, StringBuilder prefix)
+        {                        
+            if (current == p)
+            {
+                rootToP = new StringBuilder();
+                rootToP.Append(prefix.ToString());                
+            }
+
+            if (current == q)
+            {
+                rootToQ = new StringBuilder();
+                rootToQ.Append(prefix.ToString()); 
+            }
+
+            if (rootToQ.ToString().Length > 0 && rootToP.ToString().Length > 0)
+                return;
+
+            if (current.left != null)
+            {
+                var newList = new StringBuilder();
+                newList.Append(prefix.ToString());
+                newList.Append('0');
+
+                traversalPreorder(root, current.left, p, q, newList);
+            }
+
+            if (current.right != null)
+            {
+                var newList = new StringBuilder();
+                newList.Append(prefix.ToString());
+                newList.Append('1');
+
+                traversalPreorder(root, current.right, p, q, newList);
+            }
+        }
+    }
+}
