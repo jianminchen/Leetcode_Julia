1fe2b543ca881a1142dfd4788ffaba2b7dabd922
Jianmin Chen <jianminchen.fl@gmail.com>
2019-05-31 15:41:48 -0700

    check in

---
+++ By Algorithms/Leetcode 301 Remove Invalide Parentheses/2018/Remove Invalide Parentheses Submission Feb 2018.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+public class Solution
+{
+    public IList<string> RemoveInvalidParentheses(string s)
+    {
+        var validStrings = new List<string>();
+
+        if (s == null)
+        {
+            return validStrings;
+        }
+
+        var visited = new HashSet<string>();
+        var queue = new Queue<string>();
+
+        // initialize
+        queue.Enqueue(s);
+        visited.Add(s);
+
+        var found = false;
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+
+            if (isValid(current))
+            {
+                validStrings.Add(current);
+                found = true;
+            }
+
+            if (found)
+            {
+                continue;
+            }
+
+            // generate all possible states
+            for (int i = 0; i < current.Length; i++)
+            {
+                // we only try to remove left or right parenthese
+                var visit = current[i];
+                bool isParenthese = visit == '(' || visit == ')';
+
+                if (!isParenthese)
+                {
+                    continue;
+                }
+
+                // remove char stored in variable visit
+                var candidate = current.Substring(0, i) + current.Substring(i + 1);
+
+                if (!visited.Contains(candidate))
+                {
+                    queue.Enqueue(candidate);
+                    visited.Add(candidate);
+                }
+            }
+        }
+
+        return validStrings;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public static bool isValid(String s)
+    {
+        int count = 0;
+        for (int i = 0; i < s.Length; ++i)
+        {
+            var visit = s[i];
+            bool isOpen = visit == '(';
+            bool isClose = visit == ')';
+            if (isOpen)
+            {
+                ++count;
+            }
+
+            if (isClose)
+            {
+                bool noOpenToMatch = count <= 0;
+
+                if (noOpenToMatch)
+                {
+                    return false;
+                }
+
+                count--;
+            }
+        }
+
+        return count == 0;
+    }
+}
---
+++ source code/78 Subsets.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace power_set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Subsets(new int[] { 1, 2, 3 });
+        }
+
+        /// <summary>
+        /// May 31, 2019
+        /// leetcode 78: Subsets
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static IList<IList<int>> Subsets(int[] nums)
+        {
+            if (nums == null)
+                return new List<IList<int>>();
+
+            var length = nums.Length;
+            var powerSet = new List<IList<int>>(); 
+
+            for (int i = 0; i < length; i++)
+            {
+                var current = nums[i];
+
+                if (i == 0)
+                {
+                    var subsets = new List<int>();
+                    subsets.Add(current);
+
+                    powerSet.Add(new List<int>());
+                    powerSet.Add(subsets);
+                }
+                else
+                {
+                    var count = powerSet.Count;
+
+                    for (int j = 0; j < count; j++)
+                    {
+                        var list = new List<int>(powerSet[j]);
+                        list.Add(current);
+
+                        powerSet.Add(list);
+                    }
+                }
+            }
+
+            return powerSet;
+        }
+    }
+}
