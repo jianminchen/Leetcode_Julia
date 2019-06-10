2120d9b220e139695a694e4985da915c0537590e
Jianmin Chen <jianminchen.fl@gmail.com>
2019-06-10 12:11:42 -0700

    check in

---
+++ source code/1078 Occurrences After Bigram.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1078_Occurrences_after_bigram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// 1078 Occurrences After Bigram
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="first"></param>
+        /// <param name="second"></param>
+        /// <returns></returns>
+        public string[] FindOcurrences(string text, string first, string second)
+        {
+            if (text == null)
+                return new string[0];
+
+            var words = text.Split(' ');
+
+            var list = new List<string>();
+            var length = words.Length;
+            for (int i = 2; i < length; i++)
+            {
+                var firstOne = words[i - 2];
+                var secondOne = words[i - 1];
+
+                if (firstOne.CompareTo(first) == 0 && secondOne.CompareTo(second) == 0)
+                    list.Add(words[i]);
+            }
+
+            return list.ToArray();
+        }
+    }
+}
---
+++ source code/1080 insufficient nodes in root to leaf paths.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1080_insufficient_nodes_in_root_to_leaf_paths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// 1080 Insufficient Nodes in Root to Leaf Paths
+        /// Code is written in weekly contest, but failed large test case
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        public TreeNode SufficientSubset(TreeNode root, int limit)
+        {
+            var dummyParent = new TreeNode(-1);
+            dummyParent.left = root;
+
+            var path = new List<TreeNode>();
+            var parentMap = new Dictionary<TreeNode, TreeNode>();
+            parentMap.Add(root, dummyParent);
+
+            var result = preorderTraversal(root, limit, path, parentMap, 0, true);
+
+            return dummyParent.left;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="limit"></param>
+        /// <param name="path"></param>
+        /// <param name="map"></param>
+        private bool preorderTraversal(TreeNode root, int limit, List<TreeNode> path, Dictionary<TreeNode, TreeNode> map, long sum, bool isLeft)
+        {
+            if (root.left == root.right)
+            {
+                sum += root.val;
+                if (sum < limit)
+                {
+                    var parentNode = map[root];
+                    if (isLeft)
+                        parentNode.left = null;
+                    else
+                        parentNode.right = null;
+
+                    return false;
+                }
+
+                return true;
+            }
+
+            path.Add(root);
+            sum += root.val;
+
+            bool left = false;
+            bool right = false;
+
+            if (root.left != null)
+            {
+                map.Add(root.left, root);
+                left = preorderTraversal(root.left, limit, path, map, sum, true);
+            }
+
+            if (root.right != null)
+            {
+                map.Add(root.right, root);
+                right = preorderTraversal(root.right, limit, path, map, sum, false);
+            }
+
+            // back tracking
+            path.RemoveAt(path.Count - 1);
+
+            if (!(left || right) && sum < limit)
+            {
+                var parentNode = map[root];
+                if (isLeft)
+                    parentNode.left = null;
+                else
+                    parentNode.right = null;
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
