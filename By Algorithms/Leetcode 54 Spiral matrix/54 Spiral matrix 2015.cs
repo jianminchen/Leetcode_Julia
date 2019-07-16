410052698cc8deb7502ab870992586d8f5ee8146
Jianmin Chen <jianminchen.fl@gmail.com>
2019-07-16 14:18:01 -0700

    check in

---
+++ By Algorithms/Leetcode 54 Spiral matrix/54 Spiral matrix 2015.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _54_Spiral_matrix_2015_June
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// code written in June 12, 2015
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+        public IList<int> SpiralOrder(int[,] matrix)
+        {
+            IList<int> list = new List<int>();
+
+            int len1 = matrix.GetLength(0);
+            int len2 = matrix.GetLength(1);
+
+            int[][] matrix_input = new int[len1][];
+
+            for (int i = 0; i < len1; i++)
+                matrix_input[i] = new int[len2];
+
+            for (int i = 0; i < len1; i++)
+                for (int j = 0; j < len2; j++)
+                {
+                    matrix_input[i][j] = matrix[i, j];
+                }
+
+            ArrayList al = spiralOrder_2(matrix_input);
+
+            foreach (object s in al)
+            {
+                list.Add((int)s);
+            }
+            return list;
+        }
+
+        public ArrayList spiralOrder_2(int[][] matrix)
+        {
+            if (matrix == null || matrix.Length == 0 || matrix[0].Length == 0)
+                return new ArrayList();
+            return spiralOrder_3(matrix, 0, 0, matrix.Length, matrix[0].Length);
+        }
+
+        /**
+         * Latest update: June 12, 2015 
+         * Leetcode: spiral array
+         * http://gongxuns.blogspot.ca/2012/12/leetcode-spiral-matrix.html
+         * Test case: 
+         * 1. empty array 
+         * 2. one element:           1 -
+         * 2B. one row : 1 2 --
+         * 3. one column : 1 |
+         *                 2 |
+         * 4. more than 1 row, 
+         *    or more than 1 column   
+         *     1 2
+         *     3 4
+         *     output: 1 2 4 3 
+         * 5. 3 rows, 3 columns
+         *    1 2 3
+         *    4 5 6
+         *    7 8 9
+         *    output: 1 2 3 6 9 8 7 4 5 
+         * 
+         */
+        public ArrayList spiralOrder_3(int[][] matrix, int x, int y, int m, int n)
+        {
+            ArrayList res = new ArrayList();
+            // test case: empty array
+            if (m <= 0 || n <= 0) return res;
+
+            // test case 2: one row and one column
+            if (m == 1 && n == 1)
+            {
+                res.Add(matrix[x][y]);
+                return res;
+            }
+
+            //  row, from left to right 
+            for (int i = 0; i < n - 1; i++)
+            {
+                res.Add(matrix[x][y++]);
+            }
+
+            //  column, from top to down
+            for (int i = 0; i < m - 1; i++)
+            {
+                res.Add(matrix[x++][y]);
+            }
+
+            // conditional: second row, from right to left 
+            if (m > 1)
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    res.Add(matrix[x][y--]);
+                }
+            }
+
+            // conditional: second column, from bottom to top
+            if (n > 1)
+            {
+                for (int i = 0; i < m - 1; i++)
+                {
+                    res.Add(matrix[x--][y]);
+                }
+            }
+
+            // test case: one row, 1 2 3, where 1 2 is procesed above and 3 is left for next round 
+            // one column:
+            if (m == 1 || n == 1)
+            {
+                ArrayList l = spiralOrder_3(matrix, x, y, 1, 1);
+
+                foreach (object val in l)
+                    res.Add((int)val);
+            }
+            else
+            {
+                ArrayList l = spiralOrder_3(matrix, x + 1, y + 1, m - 2, n - 2);
+                foreach (object val in l)
+                    res.Add((int)val);
+            }
+
+            return res;
+        }    
+    }
+}
---
+++ By Algorithms/Leetcode 54 Spiral matrix/54 Spiral matrix 2019.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _54_Spiral_matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// online code screen - mock interview 4/12/2019             
+        public IList<int> SpiralOrder(int[][] matrix)
+        {
+            if (matrix == null || matrix.Length == 0 || matrix[0].Length == 0)
+                return new List<int>();
+
+            var rows = matrix.Length;
+            var columns = matrix[0].Length;
+
+            // right, down, left, up
+            var directionX = new int[] { 0, 1, 0, -1 };
+            var directionY = new int[] { 1, 0, -1, 0 };
+
+            var visited = new bool[rows, columns];
+
+            var direction = 0;
+            var startRow = 0;
+            var startCol = -1;
+
+            var list = new List<int>();
+
+            int index = 0;
+            while (index < rows * columns)
+            {
+                var nextRow = startRow + directionX[direction];
+                var nextCol = startCol + directionY[direction];
+
+                if (nextRow < 0 || nextRow >= rows || nextCol < 0 || nextCol >= columns || visited[nextRow, nextCol])
+                {
+                    direction = (direction + 1) % 4;
+                    continue;
+                }
+
+                list.Add(matrix[nextRow][nextCol]);
+                visited[nextRow, nextCol] = true;
+
+                startRow = nextRow;
+                startCol = nextCol;
+
+                index++;
+            }
+
+            return list;
+        }
+    }
+}
