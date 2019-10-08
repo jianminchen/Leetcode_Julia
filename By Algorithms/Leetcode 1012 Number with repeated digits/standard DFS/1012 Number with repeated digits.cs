784a8bc71d37d695b0aa6fa9b5910118e70af89a
Jianmin Chen <jianminchen.fl@gmail.com>
2019-10-08 12:19:45 -0700

    check in

---
+++ By Algorithms/Leetcode 1012 Number with repeated digits/standard DFS/1012 Number with repeated digits.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1015_number_with_repeated_digits_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumDupDigitsAtMostN(872427638);
+            Debug.Assert(value == 10);
+        }
+
+        public static int value = 0;
+        public static bool[] taken = new bool[10];
+
+        /// <summary>
+        /// March 20, 2019
+        /// study code based on ranking No. 13, VladaMG98
+        /// https://leetcode.com/contest/weekly-contest-128/ranking/
+        /// </summary>
+        /// <param name="N"></param>
+        /// <returns></returns>
+        public static int NumDupDigitsAtMostN(int N)
+        {
+            return N - getAllDifferent(N) + 1;
+        }
+
+        private static int getAllDifferent(int N)
+        {
+            value = 0;
+            for (int i = 0; i < taken.Length; i++)
+            {
+                taken[i] = false;
+            }
+
+            recursive(0, 0, N);
+            return value;
+        }
+
+        /// <summary>
+        /// Brute force solution
+        /// Make sure that all numbers in the integer are distinct, less than and equal N.
+        /// Problem statement:
+        /// Given a positive integer N, return the number of positive integers less than or 
+        /// equal to N that have at least 1 repeated digit.
+        /// Start from rightmost digit. 
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="digit"></param>
+        /// <param name="N"></param>
+        private static void recursive(double number, int digit, int N)
+        {
+            if (number > N)
+            {
+                return;
+            }
+
+            // current number with value number is the one. 
+            value += 1;
+
+            // Fix timeout bug
+            if (number * 10 > N)
+            {
+                return;
+            }
+
+            for (int x = 0; x < 10; x++)
+            {
+                if (taken[x])
+                {
+                    continue;
+                }
+
+                // make sure that the number is positive number
+                var numberIsZero = digit == 0 && x == 0;
+                if (numberIsZero)
+                {
+                    continue;
+                }
+
+                taken[x] = true;
+                recursive(number * 10 + x, digit + 1, N);
+
+                // backtracking
+                taken[x] = false;
+            }
+        }
+    }
+}
---
+++ By Algorithms/Leetcode 1012 Number with repeated digits/study Ranking No 1/1012 Number with repeated digits.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1012_number_with_repeated_digits_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = NumDupDigitsAtMostN(20);
+        }
+
+        public static int n;
+        public static int uniqueDigitis;
+
+        /// <summary>
+        /// study code on weekly contest 128 rank No. 1 waakaaka
+        /// https://leetcode.com/contest/weekly-contest-128/ranking
+        /// </summary>
+        /// <param name="N"></param>
+        /// <returns></returns>
+        public static int NumDupDigitsAtMostN(int N)
+        {
+            uniqueDigitis = 0;
+            n = N;
+            runDepthFirstSearchLeftToRight(0, 0);
+            return N + 1 - uniqueDigitis;
+        }
+
+        /// <summary>
+        /// code review March 21, 2019
+        /// Here are a few tips:
+        /// 1. using one binary number to store all unique digits in the number; mapping is described in the comment
+        /// 2. using & operator to avoid any duplicated digit
+        /// 3. using or to include current digit into the binary number
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="binaryShift"></param>
+        private static void runDepthFirstSearchLeftToRight(long value, int binaryShift)
+        {
+            if (value <= n)
+            {
+                uniqueDigitis++;
+            }
+
+            if (value * 10 > n)
+            {
+                return;
+            }
+
+            for (int digit = 0; digit <= 9; digit++)
+            {
+                // no 0 for first digit
+                if (binaryShift == 0 && digit == 0)
+                {
+                    continue;
+                }
+
+                // every number from 0 to 9 is represented using binary number
+                // 0 is expressed in 0
+                // 1 is expressed in 10
+                // 2 is expressed in 100
+                // 3 is expressed in 1000
+                // 4 is expressed in 10000
+                // 5 is expressed in 100000
+                // 6 is expressed in 1000000
+                // 7 is expressed in 10000000
+                // 8 is expressed in 100000000
+                // 9 is expressed in 1000000000
+                var currentDigitExpression = 1 << digit;
+
+                // number with unique digits, for example, 123 or 321 or 213. 
+                // all digits in the number can be expressed using one binary number 1110
+                // so next digit should avoid duplication of those three digits
+                // if current digit is 4, the exiting digits include {1, 2, 3}, then 1110 & (1 << 4) = 1110 & 10000 = 0, 
+                // since there is no duplication of digits                
+                // any digit from {1, 2, 3} & 1110 > 0 
+                if ((binaryShift & currentDigitExpression) > 0)
+                {
+                    continue;
+                }
+
+                // using | to include current digit
+                var nextBinary = binaryShift | currentDigitExpression;
+                runDepthFirstSearchLeftToRight(value * 10 + digit, nextBinary);
+            }
+        }
+    }
+}
