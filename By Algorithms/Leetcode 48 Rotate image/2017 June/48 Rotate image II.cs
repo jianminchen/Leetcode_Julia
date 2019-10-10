cfb5e7a63b0a62fbec6984d6abdac0df1e98752e
Jianmin Chen <jianminchen.fl@gmail.com>
2019-10-10 16:17:21 -0700

    check in

---
+++ By Algorithms/Leetcode 48 Rotate image/2017 June/48 Rotate image II.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _48_rotate_image___II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// submission date: 2017 June
+        /// 48 rotate image
+        /// 
+        /// </summary>
+        /// <param name="matrix"></param>
+        public void Rotate(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            int index = 0;
+            while (2 * index < rows)
+            {
+                reverseRows(index, rows - 1 - index, matrix);
+                index++;
+            }
+
+            for (int row = 0; row < rows; ++row)
+            {
+                for (int col = row + 1; col < cols; ++col)
+                {
+                    swap(row, col, matrix);
+                }
+            }
+        }
+
+        private void reverseRows(int row1, int row2, int[,] matrix)
+        {
+            int cols = matrix.GetLength(1);
+            for (int col = 0; col < cols; col++)
+            {
+                int tmp = matrix[row1, col];
+                matrix[row1, col] = matrix[row2, col];
+                matrix[row2, col] = tmp;
+            }
+        }
+
+        private void swap(int row, int col, int[,] matrix)
+        {
+            int tmp = matrix[row, col];
+            matrix[row, col] = matrix[col, row];
+            matrix[col, row] = tmp;
+        }
+    }
+}
---
+++ By Algorithms/Leetcode 48 Rotate image/2017 June/48 Rotate image.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _48_rotate_image
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// submission date: 2017 June
+        /// Leetcode rotate image
+        /// </summary>
+        /// <param name="matrix"></param>
+        public void Rotate(int[,] matrix)
+        {
+            if (matrix == null || matrix.GetLength(0) == 0 || matrix.GetLength(1) == 0)
+            {
+                return;
+            }
+
+            int rows = matrix.GetLength(0);
+            int cols = rows;
+            var rotate90 = new int[rows, cols];
+
+            int startRow = 0;
+            int startCol = 0;
+            int lastRow = rows - 1;
+            int lastCol = cols - 1;
+
+            while (startRow < rows && startCol < lastCol)
+            {
+                // put the number in the one dimension array
+                int width = lastRow - startRow + 1;
+                int circle = 4 * (width - 1);
+                var numbers = new int[circle];
+                int index = 0;
+
+                // top row
+                for (int col = startCol; col <= lastCol; col++)
+                {
+                    numbers[index++] = matrix[startRow, col];
+                }
+
+                // right col 
+                for (int row = startRow + 1; row <= lastRow; row++)
+                {
+                    numbers[index++] = matrix[row, lastCol];
+                }
+
+                // bottom row 
+                for (int col = lastCol - 1; col >= startCol; col--)
+                {
+                    numbers[index++] = matrix[lastRow, col];
+                }
+
+                // left row 
+                for (int row = lastRow - 1; row > startRow; row--)
+                {
+                    numbers[index++] = matrix[row, startCol];
+                }
+
+                // -------------
+                // Need to put numbers back to matrix                
+
+                // right col
+                int start = 0;
+                for (int row = startRow; row <= lastRow; row++)
+                {
+                    matrix[row, lastCol] = numbers[start++];
+                }
+
+                // bottom row 
+                for (int col = lastCol - 1; col >= startCol; col--)
+                {
+                    matrix[lastRow, col] = numbers[start++];
+                }
+
+                // left col 
+                for (int row = lastRow - 1; row >= startRow; row--)
+                {
+                    matrix[row, startCol] = numbers[start++];
+                }
+
+                // top row 
+                for (int col = startCol + 1; col < lastCol; col++)
+                {
+                    matrix[startRow, col] = numbers[start++];
+                }
+
+                startRow++;
+                startCol++;
+                lastRow--;
+                lastCol--;
+            }
+        }
+    }
+}
