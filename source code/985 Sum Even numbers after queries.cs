5b7557eeab51969bc551b34841076126bf32d9ba
Jianmin Chen <jianminchen.fl@gmail.com>
2019-10-25 12:07:56 -0700

    check in

---
+++ source code/985 Sum Even numbers after queries.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _985_sum_even_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// Oct. 25, 2019
+        /// 985 sum even numbers after queries
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="queries"></param>
+        /// <returns></returns>
+        public int[] SumEvenAfterQueries(int[] A, int[][] queries)
+        {
+            var rows = queries.Length;
+
+            var evenNumbersSum = new int[rows];
+
+            var sum = 0;
+            for (int j = 0; j < A.Length; j++)
+            {
+                if (A[j] % 2 == 0)
+                {
+                    sum += A[j];
+                }
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                var index = queries[i][1];
+                var value = queries[i][0];
+
+                var origin = A[index];
+                if (origin % 2 == 0)
+                {
+                    sum -= origin;
+                }
+
+                A[index] += value;
+
+                if (A[index] % 2 == 0)
+                {
+                    sum += A[index];
+                }
+
+                evenNumbersSum[i] = sum;
+            }
+
+            return evenNumbersSum;
+        }
+    }
+}
