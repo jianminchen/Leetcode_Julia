f8fe2708b61cc83b76d23589201f2646341725cd
Jianmin Chen <jianminchen.fl@gmail.com>
2019-10-29 11:53:58 -0700

    check in

---
+++ source code/1238 circular permutation.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1238_circular_permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// 1238 Circular Permutation in Binary Representation
+        /// The idea is similar to Sudoku solver; Each step using DFS to search all options. 
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="start"></param>
+        /// <returns></returns>
+        public IList<int> CircularPermutation(int n, int start)
+        {
+            var result = new List<int>();
+
+            var set = new HashSet<int>();
+            result.Add(start);
+            set.Add(start);
+
+            runDFS(n, set, result, start);
+            return result; 
+        }
+
+        /// <summary>
+        /// similar to Sudoku solver
+        /// backtracking 
+        /// DFS path - mark visited
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="hashSet"></param>
+        /// <param name="result"></param>
+        /// <param name="start"></param>
+        /// <returns></returns>
+        private static bool runDFS(int n, HashSet<int> hashSet, IList<int> result, int start)
+        {
+            if (hashSet.Count == (int)Math.Pow(2, n))
+            {
+                int x = result[result.Count - 1] ^ start; // XOR operator - first and last are different
+                return (x & (x - 1)) == 0; 
+            }
+
+            var length = result.Count;
+            int last = result[length - 1];
+
+            for (int i = 0; i < 16; i++)
+            {
+                int next = last ^ (1 << i); // XOR operator - change ith bit 
+                if (next <= (Math.Pow(2, n) - 1) && !hashSet.Contains(next))
+                {
+                    hashSet.Add(next);
+                    result.Add(next); 
+
+                    if(runDFS(n, hashSet, result, start))
+                    {
+                        return true; 
+                    }
+
+                    hashSet.Remove(next);
+                    result.RemoveAt(result.Count - 1);
+                }
+            }
+
+            return false; 
+        }
+    }
+}
---
+++ source code/989 Add To Array Form.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _989_Add_To_Array_Form
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = AddToArrayForm(new int[]{1, 2, 3, 4}, 34); 
+        }
+
+        /// <summary>
+        /// Oct 29, 2019
+        /// Leetcode 989 Add to array form
+        /// recursive solution 
+        /// First time I learn to use IEnumberable.Skip and IEnumberable.Take
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="K"></param>
+        /// <returns></returns>
+        public static IList<int> AddToArrayForm(int[] A, int K) 
+        {
+            if(K == 0)
+            {
+                return A.ToList(); 
+            }
+        
+            if(A.Length == 0 && K < 10)
+            {
+                return new int[]{K}.ToList();
+            }
+        
+            var rightmost = K % 10; 
+            var rest = K/10; 
+            var sum = 0; 
+            if(A.Length == 0)
+            {
+                sum = rightmost;
+            }
+            else 
+            {
+                sum = A[A.Length - 1] + rightmost;
+            }
+        
+            var lessThan10 = sum < 10; 
+        
+            // recursive solution 
+            IList<int> list =  new List<int>();
+            var value = rest + (lessThan10 ? 0 : 1);
+            var aLength = A.Length;
+
+            if (aLength == 0)
+            {
+                list = AddToArrayForm(new int[0], value);
+            }
+            else
+            {
+                list = AddToArrayForm(A.Take(aLength -1).ToArray(), value);
+            }
+
+            list.Add(sum % 10);
+
+            return list;                    
+        }
+    }
+}
