c53b01a1d7f4f2d233eb0b94b0d3adea2eb1b97c
Jianmin Chen <jianminchen.fl@gmail.com>
2019-11-12 14:34:04 -0800

    check in

---
+++ source code/1108 Defangle IP address.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1108_Defangle_IP_address
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// 1108 Defangle IP address
+        /// </summary>
+        /// <param name="address"></param>
+        /// <returns></returns>
+        public string DefangIPaddr(string address)
+        {
+            if (address == null || address.Length == 0)
+                return "";
+
+            var defangled = new StringBuilder();
+
+            foreach (var item in address)
+            {
+                if (item == '.')
+                {
+                    defangled.Append("[.]");
+                }
+                else
+                {
+                    defangled.Append(item);
+                }
+            }
+
+            return defangled.ToString();
+        }
+    }
+}
