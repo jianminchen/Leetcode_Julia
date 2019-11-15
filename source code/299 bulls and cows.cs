b6698c380647628c9f9c6b1203f6b2973b865cc2
Jianmin Chen <jianminchen.fl@gmail.com>
2019-11-14 17:18:48 -0800

    check in

---
+++ source code/299 bulls and cows.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _299_bulls_and_cows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // secret = "1807", guess = "7810"
+            var result = GetHint("1807","7810"); 
+        }
+
+        /// <summary>
+        /// 299 bulls and cows
+        /// 
+        /// </summary>
+        /// <param name="secret"></param>
+        /// <param name="guess"></param>
+        /// <returns></returns>
+        public static string GetHint(string secret, string guess)
+        {
+            var secretCount = countDigits(secret);
+            var guessCount  = countDigits(guess);
+
+            var total = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                var real = secretCount[i];
+                var guessed = guessCount[i];
+                
+                total += Math.Min(real, guessed);                
+            }
+
+            var bullCount = 0;
+            for (int i = 0; i < secret.Length; i++)
+            {
+                if (secret[i] == guess[i])
+                {
+                    bullCount++; 
+                }
+            }
+
+            var cowCount = total - bullCount;
+            return bullCount.ToString() + "A" + cowCount.ToString() + "B";
+        }
+
+        private static int[] countDigits(string word)
+        {
+            var count = new int[10];
+            foreach (var item in word)
+            {
+                int number = item - '0';
+                count[number]++;
+            }
+
+            return count; 
+        }
+    }
+}
