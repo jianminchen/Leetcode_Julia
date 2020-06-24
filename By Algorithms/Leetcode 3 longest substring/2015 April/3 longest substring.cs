1eb023ee7edbaf7847c982f46a762d098e4c42b0
Jianmin Chen <jianminchen.fl@gmail.com>
2020-06-24 16:59:00 -0700

    check in

---
+++ By Algorithms/Leetcode 3 longest substring/2015 April/3 longest substring.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_longest_substring_2015_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// code review on June 24, 2020
+        /// code written on April 2015
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public int LengthOfLongestSubstring(string s)
+        {
+
+            int start = 0, end = 0;
+            int maxLen = 0;
+            int maxStart = 0, maxEnd = 0;
+
+            bool setOneMaximum = false;
+
+            int[] lastSeen = new int[256];
+
+            for (int i = 0; i < 256; i++)
+                lastSeen[i] = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char cur = s[i];
+
+                bool firstVisit = lastSeen[cur] == 0;
+
+                if (firstVisit)
+                {
+                    //first time to visit cur, add it to the array, keep the log
+                    lastSeen[cur]++;
+                    end++;
+
+                    int lastOne = s.Length - 1;
+                    bool isLastOne = (i == lastOne);
+                    if (isLastOne)
+                    {
+                        // need to compare the one with setOneMaximum
+                        // test case: string testStr = "abcdcefg";   
+                        // string ends with non-duplicate char
+                        if (setOneMaximum)
+                        {
+                            int curLen = end - start;
+                            bool curLonger = curLen > maxLen;
+                            if (curLonger)
+                            {
+                                maxLen = curLen;
+                                maxStart = start;
+                                maxEnd = end;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    // this is duplicate one. Second time to visit 
+                    // 1. First thing, compare to the largest one
+                    char duplicateCh = cur;
+                    end++;
+                    if (setOneMaximum)
+                    {
+                        // need to compare the current one with maximum one, and then keep the longer one
+                        int curLen = end - start - 1;
+                        bool curLonger = curLen > maxLen;
+                        if (curLonger)
+                        {
+                            maxLen = curLen;
+                            maxStart = start;
+                            maxEnd = end;
+                        }
+                    }
+                    else
+                    {
+                        // First time, need to set up maximum one 
+                        setOneMaximum = true;
+                        maxLen = end - start - 1;   // bug2: end-start-1, not end-start
+                        maxStart = start;
+                        maxEnd = end;
+                    }
+
+                    // Need to move the start postion of new possible string 
+                    int startPos = start;
+                    for (int j = startPos; j < end; j++)
+                    {
+                        char removeCh = s[j];
+                        if (s[j] != duplicateCh)
+                        {
+                            lastSeen[removeCh] = 0;  // bug: removeCh, not j
+                            start++;
+                        }
+
+                        if (removeCh == duplicateCh)  // remove the first duplicated char as well, then stop 
+                        {
+                            lastSeen[removeCh] = 1;   // bug: removeCh, not j
+                            start++;  // make sure to adjust the start position
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // For this case, no duplicate char in the string - only do the work once
+            if (!setOneMaximum)
+            {
+                maxLen = s.Length;
+                maxStart = 0;
+                maxEnd = s.Length;
+            }
+
+            return maxLen;
+        }
+    }
+}
---
+++ By Algorithms/Leetcode 3 longest substring/2015 May/3 longest substringcs.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_longest_substring_2015_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// code review on June 24, 2020
+        /// code written in May 2015
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public int LengthOfLongestSubstring(string s)
+        {
+            int len = s.Length;
+
+            if (s == null || len == 0)
+            {
+                return 0;
+            }
+
+            // visited[char's ASCII] = char's index  
+            int[] visited = new int[256];
+            arraysFill(visited, -1);
+
+            int sw_len = 1;
+            int maxLen = 1;
+            int lastVisit_index = 0;
+            visited[s[0]] = 0;
+            int sw_start = 0;
+            for (int i = 1; i < len; i++)
+            {
+                lastVisit_index = visited[s[i]];
+
+                bool isInSlideWindow = lastVisit_index >= sw_start;
+                if (lastVisit_index == -1 || !isInSlideWindow)
+                {
+                    // do nothing  // test case: baab
+                }
+                else
+                {
+                    // julia comment: 
+                    // before sliding window left sw_start moves to next position, 
+                    // record the length of sliding window, compared to 
+                    // maxLen; update it if need
+                    // test case: aba - 012 
+                    // sw_len = 2 - 0 = 2, sw_start = 1  
+                    sw_len = i - sw_start;
+                    maxLen = Math.Max(maxLen, sw_len);
+
+                    // then, move sliding window left point 
+                    sw_start = lastVisit_index + 1;
+                }
+
+                // julia comment: update visited extra space - array 
+                // test case: aba, visit['a'] = 2
+                visited[s[i]] = i;
+
+                // test case: aba, sw_len = 2 
+                sw_len = i - sw_start + 1;
+            }
+
+            // 最后一次  
+            maxLen = Math.Max(maxLen, sw_len);
+
+            return maxLen;
+        }
+
+        public void arraysFill(int[] a, int val)
+        {
+            for (int i = 0; i < 256; i++)
+                a[i] = val;
+        }
+    }
+}
