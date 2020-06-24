eb57cc68c88076c4bf19129786828a7b0fa223d0
Jianmin Chen <jianminchen.fl@gmail.com>
2020-06-24 13:53:42 -0700

    check in

---
+++ source code/208 Implement Trie.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _208_implement_trie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public class Trie
+        {
+
+
+            private Trie[] Children = new Trie[26];
+            private string word;
+
+            /** Initialize your data structure here. */
+            public Trie()
+            {
+                //Children = new Trie[26];
+
+            }
+
+            /** Inserts a word into the trie. */
+            public void Insert(string word)
+            {
+                if (word == null || word.Length == 0)
+                    return;
+
+                var root = this;
+
+                foreach (var item in word)
+                {
+                    if (root.Children[item - 'a'] == null)
+                    {
+                        root.Children[item - 'a'] = new Trie();
+                    }
+
+                    root = root.Children[item - 'a'];
+                }
+
+                root.word = word;
+            }
+
+            /** Returns if the word is in the trie. */
+            public bool Search(string word)
+            {
+                if (word == null || word.Length == 0)
+                    return false;
+
+                var root = this;
+
+                foreach (var item in word)
+                {
+                    if (root.Children[item - 'a'] == null)
+                    {
+                        return false;
+                    }
+
+                    root = root.Children[item - 'a'];
+                }
+
+                return root.word != null;
+            }
+
+            /** Returns if there is any word in the trie that starts with the given prefix. */
+            public bool StartsWith(string prefix)
+            {
+                if (prefix == null || prefix.Length == 0)
+                    return false;
+
+                var root = this;
+
+                foreach (var item in prefix)
+                {
+                    if (root.Children[item - 'a'] == null)
+                    {
+                        return false;
+                    }
+
+                    root = root.Children[item - 'a'];
+                }
+
+                return true;
+            }
+        }
+    }
+}
---
+++ source code/46 Permutation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = Permute(new int[] { 1, 2});
+        }
+
+        public static IList<IList<int>> Permute(int[] nums)
+        {
+            if (nums == null)
+                return new List<IList<int>>();
+
+            var permutations = new List<IList<int>>();
+            var set = new HashSet<int>();
+            var list = new List<int>();
+
+            runDFS(nums, set, list, permutations);
+
+            return permutations;
+        }
+
+        //[1]
+        //[1, 2]
+        //[1, 2, 3]
+        private static void runDFS(int[] numbers, HashSet<int> found, IList<int> list, List<IList<int>> result)
+        {
+            if (numbers.Length == list.Count) // true
+            {
+                var copy = new List<int>();
+                foreach (var item in list)
+                    copy.Add(item);
+
+                result.Add(copy);// {1, 2}
+                return;
+            }
+
+            for (int i = 0; i < numbers.Length; i++) //i = 1
+            {
+                var current = numbers[i]; // 2
+                if (found.Contains(current)) //false
+                {
+                    continue;
+                }
+
+                // DFS 
+                list.Add(current);// [1,2
+                found.Add(current); // {1,2}
+
+                runDFS(numbers, found, list, result); // 
+
+                list.RemoveAt(list.Count - 1); // not remove, should be RemoveAt
+                found.Remove(current); // remove 2           
+            }
+        }
+    }
+}
