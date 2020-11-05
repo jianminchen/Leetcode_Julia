74089684b018f7607b2f21eeba4ce4614827609e
Jianmin Chen <jianminchen.fl@gmail.com>
2020-11-04 16:06:41 -0800

    check in

--- By Algorithms/Leetcode 940 distinct subsequence/940 distinct subsequence.cs
+++ By Algorithms/Leetcode 940 distinct subsequence/940 distinct subsequence.cs
@@ -4,63 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _940_distinct_subsequences
+namespace _940_distinct_subsequence_II
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var result = DistinctSubseqII("abc");
         }
 
         /// study code
         /// https://www.geeksforgeeks.org/count-distinct-subsequences/
-        public int DistinctSubseqII(string str) {        
-            // Returns count of distinct sunsequences of str.     
-            // Create an array to store index
-            // of last
-            var MAX_CHAR = 256;
+        /// I could not make it work, so I studid another one
+        /// https://leetcode.com/problems/distinct-subsequences-ii/discuss/560060/Simple-C-DP-Solution
+        public static int DistinctSubseqII(string s)
+        {
+            long MOD    = 1000 * 1000 * 1000 + 7; 
+            long result = 0;
+            var  length = s.Length;
 
-            int[] last = new int[MAX_CHAR];
-            int mod = 1000 * 1000 * 1000 + 7; 
+            var dp      = new long[length + 1];
+            var visited = new bool[length + 1, 26];
 
-            for (int i = 0; i < MAX_CHAR; i++)
-            {
-                last[i] = -1;
-            }
- 
-            // Length of input string
-            int n = str.Length;
- 
-            // dp[i] is going to store count of
-            // distinct subsequences of length i.
-            var dp = new long[n + 1];
- 
-            // Empty substring has only one subsequence
             dp[0] = 1;
- 
-            // Traverse through all lengths from 1 to n.
-            for (int i = 1; i <= n; i++) 
+
+            for (int index = 1; index <= s.Length; index++)
             {
-                // Number of subsequences with substring
-                // str[0..i-1]
-                dp[i] = (2 * dp[i - 1]) % mod;
- 
-                // If current character has appeared
-                // before, then remove all subsequences
-                // ending with previous occurrence.
-                var lastDigit = (int)str[i - 1];
-                var index = last[lastDigit];
-            
-                if (index != -1)
+                var endIndex = s[index - 1] - 'a';
+                    
+                for (int start = 0; start < index; start++)
                 {
-                    dp[i] = (dp[i] - dp[index]) % mod;
+                    // think about the following ways:
+                    // distinct subsequence with length = start -> dp[start] (not end with endIndex)
+                    // ...
+                    if (!visited[start, endIndex])
+                    {
+                        visited[start, endIndex] = true;
+
+                        dp[index] += dp[start];
+                        result    += dp[start];
+
+                        dp[index] %= MOD;
+                        result    %= MOD;
+                    }
                 }
- 
-                // Mark occurrence of current character
-                last[(int)str[i - 1]] = (i - 1);
             }
-        
-            return (int)(dp[n] - 1);
+               
+            return (int)result;
         }
     }
 }
